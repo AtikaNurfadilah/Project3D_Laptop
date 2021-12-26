38ffb0ca27e53e7bf5b4e3ac33dd4c12e7a4f028
NurRahmawatiSubuh <nurrahmaws182@gmail.com>
2021-12-25 23:05:31 -1200

    tambahan rahma

--- main.cpp
+++ main.cpp
@@ -20,3 +20,29 @@
     }
 
     float _angle = -70.0f;
+
+        //Draws the 3D scene
+    void drawScene() {
+     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+     glMatrixMode(GL_MODELVIEW); // keep it like this
+     glLoadIdentity();
+
+     glTranslatef(0.0f, 0.0f, -14.0f);
+
+     //Add ambient light
+     GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+
+     //Add positioned light
+     GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
+     GLfloat lightPos0[] = {0.0f, -8.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+     glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+
+     //Add directed light
+     GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
+     //Coming from the direction (-1, 0.5, 0.5)
+     GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
+     glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+     glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
