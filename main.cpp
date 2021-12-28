9639105175801bf1d9ce2c77596815aa8b944d0d
dhaniahmad55 <dhanpratama55@gmail.com>
2021-12-27 19:52:42 -1200

    tambahan dari dhani

--- main.cpp
+++ main.cpp
@@ -164,3 +164,33 @@
      glEnd();
      glutSwapBuffers();
     }
+    void update(int value) {
+     _angle += 1.5f;
+     if (_angle > 360) {
+      _angle -= 360;
+     }
+
+     glutPostRedisplay();
+     glutTimerFunc(25, update, 0);
+    }
+
+    int main(int argc, char** argv) {
+     //Initialize GLUT
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+     glutInitWindowSize(600, 600);
+
+     //Create the window
+     glutCreateWindow("Lighting");
+     initRendering();
+
+     //Set handler functions
+     glutDisplayFunc(drawScene);
+
+     glutReshapeFunc(handleResize);
+
+     update(0);
+
+     glutMainLoop();
+     return 0;
+    }
